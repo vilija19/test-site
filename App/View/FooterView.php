633586a95633eec59ee62823ada51f8fe52a30b1
zan <billibons777@gmail.com>
2023-02-20 19:41:53 +0000

    Added EOL

--- App/View/FooterView.php
+++ App/View/FooterView.php
@@ -12,14 +12,14 @@ class FooterView implements \Vilija19\Core\Interfaces\RenderInterface
     }
     public function render()
     {
-        $view = '   </div>';
-        $view .= '  <footer>';
-        $view .= '      <div class="container"><div class="row align-items-center">';
-        $view .= '          <div class="col text-center"><p>'.$this->data['footer_text'].'</p></div>';
-        $view .= '      </div></div>';
-        $view .= '  </footer>';
-        $view .= '</body>';
-        $view .= '</html>';
+        $view = '   </div>'.PHP_EOL;
+        $view .= '  <footer>'.PHP_EOL;
+        $view .= '      <div class="container"><div class="row align-items-center">'.PHP_EOL;
+        $view .= '          <div class="col text-center"><p>'.$this->data['footer_text'].'</p></div>'.PHP_EOL;
+        $view .= '      </div></div>'.PHP_EOL;
+        $view .= '  </footer>'.PHP_EOL;
+        $view .= '</body>'.PHP_EOL;
+        $view .= '</html>'.PHP_EOL;
 
 
         echo $view;
--- App/View/HomeView.php
+++ App/View/HomeView.php
@@ -13,33 +13,33 @@ class HomeView implements \Vilija19\Core\Interfaces\RenderInterface
     public function render()
     {
 
-        $view = '  <div class="row align-items-center">';
-        $view .= '      <div class="col-sm-10"><h1>' . $this->data['title']  . '</h1></div>';
-        $view .= '      <div class="col-sm-2">';
-        $view .= '          <a href="/add-product" role="button" class="btn btn-primary">ADD</a>';
-        $view .= '          <button type="button" id="delete-product-btn" class="btn btn-danger">MASS DELETE</button>';
-        $view .= '      </div>';
-        $view .= '  </div>';
-        $view .= '  <div class="row border-top">';
+        $view = '  <div class="row align-items-center">'.PHP_EOL;
+        $view .= '      <div class="col-sm-10"><h1>' . $this->data['title']  . '</h1></div>'.PHP_EOL;
+        $view .= '      <div class="col-sm-2">'.PHP_EOL;
+        $view .= '          <a href="/add-product" role="button" class="btn btn-primary">ADD</a>'.PHP_EOL;
+        $view .= '          <button type="button" id="delete-product-btn" class="btn btn-danger">MASS DELETE</button>'.PHP_EOL;
+        $view .= '      </div>'.PHP_EOL;
+        $view .= '  </div>'.PHP_EOL;
+        $view .= '  <div class="row border-top">'.PHP_EOL;
 
         foreach ($this->data['products'] as $product) {
-            $view .= '      <div class="col-sm-2 border mt-4 pb-5">';
-            $view .= '          <div class="row align-items-center"><div class="col">';
-            $view .= '              <input class="form-check-input delete-checkbox" name="select-product[]" type="checkbox" value="'.$product->id.'">';
-            $view .= '          </div></div>';
-            $view .= '          <div class="row"><div class="col text-center">'.$product->sku.'</div></div>';
-            $view .= '          <div class="row"><div class="col text-center">'.$product->name.'</div></div>';
-            $view .= '          <div class="row"><div class="col text-center">'.$product->price.' $</div></div>';
+            $view .= '      <div class="col-sm-2 border mt-4 pb-5">'.PHP_EOL;
+            $view .= '          <div class="row align-items-center"><div class="col">'.PHP_EOL;
+            $view .= '              <input class="form-check-input delete-checkbox" name="select-product[]" type="checkbox" value="'.$product->id.'">'.PHP_EOL;
+            $view .= '          </div></div>'.PHP_EOL;
+            $view .= '          <div class="row"><div class="col text-center">'.$product->sku.'</div></div>'.PHP_EOL;
+            $view .= '          <div class="row"><div class="col text-center">'.$product->name.'</div></div>'.PHP_EOL;
+            $view .= '          <div class="row"><div class="col text-center">'.$product->price.' $</div></div>'.PHP_EOL;
             foreach ($product->attributes as $attribute) {
-            $view .= '          <div class="row"><div class="col text-center">';
+            $view .= '          <div class="row"><div class="col text-center">'.PHP_EOL;
             $view .= '              ' . $attribute->name . ': ' . $attribute->value . ' ' . $attribute->attribute->value_unit;
-            $view .= '          </div></div>';
+            $view .= '          </div></div>'.PHP_EOL;
             }
-            $view .= '      </div>';
+            $view .= '      </div>'.PHP_EOL;
         }
 
-        $view .= '  </div>';
-        $view .= '</div>';
+        $view .= '  </div>'.PHP_EOL;
+        $view .= '</div>'.PHP_EOL;
 
         echo $view;
     }
